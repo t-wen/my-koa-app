7c5670603041c03c87c32351005666cee812b978
t-wen <30753500+t-wen@users.noreply.github.com>
2018-07-11 16:49:09 +0800

    update

--- koa.js
+++ koa.js
@@ -1,6 +1,13 @@
+const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
-app.use(async ctx =>{
-	ctx.body = 'Hello World';
-});
+ 
+// $ GET /hello.txt
+app.use(serve('./'));
+ 
+// or use absolute paths
+//app.use(serve(__dirname + '/test/fixtures'));
+ 
 app.listen(3000);
+ 
+console.log('listening on port 3000');
